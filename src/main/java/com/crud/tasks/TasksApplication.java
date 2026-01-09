80ffdb8654b72de2cb9b6d074729d73512f5edba
Artur Flisek <artur.flisek@pentacomp.pl>
2026-01-09 15:53:21 +0100

    14.3 Dodanie zadania dodatkowego

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,11 +3,53 @@ package com.crud.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class TasksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
+//		SpringApplication.run(TasksApplication.class, args);
+
+        List<Integer> grades = new ArrayList<>(List.of(3, 1, 1, 5, 6, 4));
+        List<Integer> weights = new ArrayList<>(List.of(4, 6, 8, 4, 4, 10));
+
+        weightAverage(grades, weights);
+
 	}
+    private static void weightAverage(List<Integer> grades, List<Integer> weights) {
+        if (grades.isEmpty() || weights.isEmpty()) {
+            throw new Error("List of grades and weights cannot be empty");
+        }
+
+        if (grades.size() != weights.size()) {
+            throw new Error("Incorrect number of grades and weights");
+        }
+
+        for(int grade : grades) {
+            if(grade < 1 || grade > 6) {
+                throw new Error("Incorrect grade value");
+            }
+        }
+
+        for(int weight : weights) {
+            if(weight < 1 || weight > 10) {
+                throw new Error("Incorrect weight value");
+            }
+        }
+
+        BigDecimal sum = BigDecimal.ZERO;
+        BigDecimal weightSum = BigDecimal.ZERO;
+        for(int i = 0; i < grades.size(); i++) {
+            sum = sum.add(BigDecimal.valueOf(grades.get(i)).multiply(BigDecimal.valueOf(weights.get(i))));
+            weightSum = weightSum.add(BigDecimal.valueOf(weights.get(i)));
+        }
+
+        System.out.println("Average weight: " + sum.divide(weightSum, 2, RoundingMode.HALF_UP));
+    }
 
 }
