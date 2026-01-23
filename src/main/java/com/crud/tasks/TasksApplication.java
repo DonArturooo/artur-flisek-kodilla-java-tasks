515871eb728ce0af155eb85c7846676964d7501f
Artur Flisek <artur.flisek@pentacomp.pl>
2026-01-23 19:53:39 +0100

    17.2 zadanie dodatkowe

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -23,22 +24,23 @@ public class TasksApplication {
             throw new Error("Incorrect number of grades and weights");
         }
 
-        for(int grade : grades) {
-            if(grade < 1 || grade > 6) {
+        for (int grade : grades) {
+            if (grade < 1 || grade > 6) {
                 throw new Error("Incorrect grade value");
             }
         }
 
-        for(int weight : weights) {
-            if(weight < 1 || weight > 10) {
+        for (int weight : weights) {
+            if (weight < 1 || weight > 10) {
                 throw new Error("Incorrect weight value");
             }
         }
 
         BigDecimal sum = BigDecimal.ZERO;
         BigDecimal weightSum = BigDecimal.ZERO;
-        for(int i = 0; i < grades.size(); i++) {
-            sum = sum.add(BigDecimal.valueOf(grades.get(i)).multiply(BigDecimal.valueOf(weights.get(i))));
+        for (int i = 0; i < grades.size(); i++) {
+            sum = sum.add(BigDecimal.valueOf(grades.get(i))
+                                    .multiply(BigDecimal.valueOf(weights.get(i))));
             weightSum = weightSum.add(BigDecimal.valueOf(weights.get(i)));
         }
 
@@ -46,12 +48,12 @@ public class TasksApplication {
     }
 
     public static boolean isFirstNumber(int number) {
-        if(number <= 1) {
+        if (number <= 1) {
             return false;
         }
 
         for (int i = 2; i < number; i++) {
-            if(number % i == 0) {
+            if (number % i == 0) {
                 return false;
             }
         }
@@ -60,7 +62,32 @@ public class TasksApplication {
     }
 
     public static List<Integer> segregate(List<Integer> numbers) throws Exception {
-        if(numbers.isEmpty()) throw new Exception("Pusta lista");
-        return numbers.stream().sorted(Comparator.comparingInt(a -> a)).toList();
+        if (numbers.isEmpty()) {
+            throw new Exception("Pusta lista");
+        }
+        return numbers.stream()
+                      .sorted(Comparator.comparingInt(a -> a))
+                      .toList();
+    }
+
+    public static List<Integer> segregateWithGas(List<Integer> numbers) throws Exception {
+        if (numbers.isEmpty()) {
+            throw new Exception("Pusta lista");
+        }
+        List<Integer> sorted = new ArrayList<>(numbers);
+
+        for (int j = 0; j < sorted.size(); j++) {
+            for (int i = 0; i < sorted.size() - 1 - j; i++) {
+                int first = sorted.get(i);
+                int second = sorted.get(i + 1);
+
+                if (first > second) {
+                    sorted.set(i + 1, first);
+                    sorted.set(i, second);
+                }
+
+            }
+        }
+        return sorted;
     }
 }
