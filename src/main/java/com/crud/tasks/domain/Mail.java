7361f5b2e408e1f09d36711a6aecf732da2a7991
Artur Flisek <artur.flisek@pentacomp.pl>
2026-02-01 00:52:40 +0100

    19.3 Dodanie materiałów i zadania

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+@Builder
+public class Mail{
+    private final String mailTo, subject, message;
+
+    @Builder.Default
+    private final Optional<String> toCc = Optional.empty();
+}
