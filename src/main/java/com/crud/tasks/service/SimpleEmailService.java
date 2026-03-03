596002740d0b885c554589ce37fb69e36df117f9
Artur Flisek <artur.flisek@gmail.com>
2026-03-03 23:36:00 +0100

    24.2 Dodanie materiałów i zadania

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,9 +3,12 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,10 +18,13 @@ public class SimpleEmailService {
 
     private final JavaMailSender javaMailSender;
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
         try {
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             log.info("Email has been sent.");
         } catch (MailException exception) {
             log.error("Failed to process email sending: " + exception.getMessage(), exception);
@@ -34,5 +40,17 @@ public class SimpleEmailService {
         if(mail.getToCc().isPresent())
             mailMessage.setCc(mail.getToCc().get());
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail){
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom(mail.getMailFrom());
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+            if(mail.getToCc().isPresent())
+                messageHelper.setCc(mail.getToCc().get());
+        };
     }
 }
